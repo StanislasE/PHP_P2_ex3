a1dfe32d4582af91466c3c08792a77c014dfa8b9
StanislasE <stanislas.etienne@gmail.com>
2020-11-19 10:24:56 +0100

    fait

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 3</title>
+</head>
+<body>
+    
+<?php
+    $gender = 'Homme' ;
+    
+    if ($gender == 'Homme'){
+        echo 'C\'est un développeur !!!!' ;
+    }
+    else {
+        echo 'C\'est une développeuse !!!!' ;
+    }
+?>
+
+</body>
+</html>
